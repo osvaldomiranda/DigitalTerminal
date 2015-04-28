0b970b3383d3e96b7aee15ca1ac8c57f2ca08836
osvaldomiranda <osvaldo.omiranda@gmail.com>
2015-04-28 07:41:31 -0400

    [ADD] migrations class and create table USER

---
+++ DigitalTerminal/Migrations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.Odbc;
+
+namespace DigitalTerminal
+{
+    class Migrations
+    {
+        public void migrate()
+        {
+            Connect con = new Connect();
+            OdbcConnection conexion = con.getConnect();
+
+            //**********************************************
+            // USER
+            OdbcCommand createUser = new OdbcCommand();
+            createUser.Connection = conexion;
+            createUser.CommandText = "CREATE TABLE IF NOT EXIST \"user\"" +
+            "(" +
+            "    email character varying(60) NOT NULL," +
+            "    password character varying(12)," +
+            "    CONSTRAINT email PRIMARY KEY (email)" +
+            ")" +
+            "WITH (" +
+            "    OIDS=FALSE" +
+            ");" +
+            "ALTER TABLE \"user\"" +
+            "OWNER TO postgres;";
+            createUser.ExecuteNonQuery();
+            conexion.Close();
+        }
+    }
+}
